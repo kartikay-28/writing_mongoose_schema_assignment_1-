c8858dbc0492e4ef002b8a1973aee5eaaefbc590
kartikay-28 <kartikayrattan1@gmail.com>
2025-02-15 09:45:06 +0530

    done

---
+++ schema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  roles: {
+    type: [String],
+    default: ['user']
+  },
+  profile: {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    age: {
+      type: Number,
+      required: true
+    }
+  },
+  lastLogin: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
